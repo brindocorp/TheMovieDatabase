3a9fa9ed90bd11c5bb47935f598ebb011dcb8962
femoxmed <femoxmed@gmail.com>
2020-02-29 20:50:58 +0100

    background design

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import App from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "../public/scss/app.scss";
 // import { Button } from "next-element-react/next";
 class MyApp extends App {
   render() {
