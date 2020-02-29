667308971fe8a2863712a48122daebed6baca1d7
femoxmed <femoxmed@gmail.com>
2020-02-29 19:37:11 +0100

    set up client for the movie app

---
+++ client/pages/_app.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import App from "next/app";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+// import { Button } from "next-element-react/next";
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
+}
+
+export default MyApp;
