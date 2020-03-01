792c518900aa5a2914d508c302d423a98fbb2e59
femoxmed <femoxmed@gmail.com>
2020-03-01 08:03:11 +0100

    styling the trending movies 20%

--- client/components/MovieCards.js
+++ client/components/MovieCards.js
@@ -3,30 +3,35 @@ import React from "react";
 
 const MovieCard = () => (
   <div>
+    <div className="row mx-1">
+      <figure
+        className="ti-grid-img width-400 bg-cover position-bg"
+        style={{
+          background:
+            'url("/cinema_bg2.jpg");height: 300px;width: 300px;background-size: cover; margin:0'
+        }}
+      ></figure>
+
+      <div className="d-flex w-100 movie-card-bottom p-2 justify-content-between">
+        <div>
+          <p className="lead font-weight-bold"> Logan</p>
+          <p className="small">Action , Adventory, Fantasy</p>
+        </div>
+
+        <div className="movie-card-rating">
+          <div className="p-3">4.0 </div>
+        </div>
+      </div>
+    </div>
     <style jsx>{`
-      :global(body) {
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
-          Helvetica, sans-serif;
+      .movie-card-bottom {
+        border: 1px solid grey;
       }
-      nav {
-        text-align: center;
+
+      .movie-card-genre {
       }
-      ul {
-        display: flex;
-        justify-content: space-between;
-      }
-      nav > ul {
-        padding: 4px 16px;
-      }
-      li {
-        display: flex;
-        padding: 6px 8px;
-      }
-      a {
-        color: #067df7;
-        text-decoration: none;
-        font-size: 13px;
+      .movie-card-rating {
+        border: 1px solid red;
       }
     `}</style>
   </div>
