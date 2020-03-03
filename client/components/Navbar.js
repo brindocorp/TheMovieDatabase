bed67d60dc9305165d55c61302c36bb4e9013621
femoxmed <femoxmed@gmail.com>
2020-03-03 13:20:52 +0100

    last updates

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,14 +1,21 @@
 import React from "react";
 import Link from "next/link";
 import { Button } from "semantic-ui-react";
-const Nav = () => (
-  <div className="container-fluid position-absolute zIndex">
+const background = {
+  background: "blue"
+};
+const Nav = props => (
+  <div
+    className="container-fluid position-absolute zIndex my-auto"
+    style={props.bg ? { background: "#889590" } : { background: "transparent" }}
+  >
     <div className="row ">
-      <div className="col d-flex justify-content-between mt-3 ">
-        <div className="div text-white logo-font-size d-flex">
-          {" "}
-          <span> MOVIE</span> <span className="font-weight-bold">BOX</span>
-        </div>
+      <div className="col d-flex justify-content-between py-2 my-auto ">
+        <Link href="/">
+          <div className="div text-white logo-font-size d-flex cursor-pointer">
+            <span> MOVIE</span> <span className="font-weight-bold">BOX</span>
+          </div>
+        </Link>
         <div className="div d-flex text-white justify-content-between">
           <Link href="/signin">
             <Button basic inverted className="mr-2 mr-lg-3">
